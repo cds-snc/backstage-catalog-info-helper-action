e3590f02cd0c10f89b5bcc64349986ef333ee173
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2023-10-19 12:39:58 +0000

    fix: cleanup query functions
    feat: add base catalog functions
    
    feat: add debug script
    
    feat: update dependencies and add scripts
    
    fix: revert workflow changes
    
    feat: add pre-commit hook
    
    temp: comment out base script
    
    feat: setup unit tests
    
    fix: add missing env jest
    
    fix: update function to return empty array on 404
    
    fix: clean up functions
    
    fix: clean up debug script
    
    feat: setup unit tests for default flow
    
    fix: default flow for action
    
    fix: switch to console log
    
    fix: add missing unit tests
    
    fix: lint
    
    chore: run pre-commit

---
+++ debug/debug.js
@@ -0,0 +1,125 @@
+"use strict";
+
+const fs = require("fs");
+const github = require("@actions/github");
+const { Octokit } = require("@octokit/rest");
+const { createAppAuth } = require("@octokit/auth-app");
+const { queryRepository, queryTeamsForRepository } = require("../src/query.js");
+const {
+  hasCatalogInfo,
+  parseCatalogInfo,
+  getEntityOwners,
+  generateCatalogInfo,
+  saveCatalogInfo,
+} = require("../src/catalog.js");
+
+const octokitAppAuth = new Octokit({
+  authStrategy: createAppAuth,
+  auth: {
+    appId: process.env.GH_APP_ID,
+    privateKey: process.env.GH_APP_PRIVATE_KEY,
+  },
+});
+
+const getInstallationId = async () => {
+  const { data: installations } = await octokitAppAuth.apps.listInstallations();
+  const installation = installations.find(
+    (installation) => installation.account.login === "cds-snc"
+  );
+  return installation.id;
+};
+
+const run = async () => {
+  console.log("Getting installation ID...");
+  const installationId = await getInstallationId();
+  console.log(installationId);
+
+  const auth = createAppAuth({
+    appId: process.env.GH_APP_ID,
+    privateKey: process.env.GH_APP_PRIVATE_KEY,
+  });
+
+  const installationAuthentication = await auth({
+    type: "installation",
+    installationId,
+  });
+  const octokit = github.getOctokit(installationAuthentication.token);
+
+  const owner = github.context.repo.owner;
+  const repo = github.context.repo.repo;
+
+  console.log(`Owner: ${owner}\nRepo: ${repo}`);
+  console.log(" Repository data sent to Azure Log Analytics");
+
+  console.log("✅ Generating catalog-info.yaml...");
+  // get current repository data
+  const repository = await queryRepository(octokit, owner, repo);
+  console.log("Repository data");
+  console.log("===============");
+  console.log(repository);
+  console.log(repository);
+
+  // get repository teams
+  console.log(`👥 Getting teams for ${owner}/${repo}...`);
+  const teams = await queryTeamsForRepository(octokit, owner, repo);
+  console.log("Teams");
+  console.log("=====");
+  console.log(teams);
+
+  // get repository team permissions
+  if (teams.length === 0) {
+    console.log("No teams found");
+  } else {
+    console.log("Teams permissions");
+    console.log("================\n");
+    for (const team of teams) {
+      console.log("----");
+      console.log("name: " + team.name);
+      console.log("permission: " + team.permission + "\n");
+    }
+  }
+
+  // get entity owners
+  const entityOwners = await getEntityOwners(teams);
+  console.log("Entity owners");
+  console.log("=============");
+  console.log(entityOwners);
+
+  // check if catalog-info.yaml exists on root of repository
+  console.log("Catalog info file exists?");
+  console.log("========================");
+  const hasCatalogInfoFile = await hasCatalogInfo();
+  console.log(hasCatalogInfoFile);
+
+  if (hasCatalogInfoFile) {
+    // if file exists, delete it
+    console.log("Deleting catalog-info.yaml");
+    fs.rmSync("catalog-info.yaml");
+  }
+  // generate catalog-info.yaml
+  console.log("✅ Generating catalog-info.yaml...");
+  console.log("============================");
+  const catalogInfoContent = await generateCatalogInfo(repository, teams);
+  console.log(catalogInfoContent);
+
+  // save catalog-info.yaml
+  console.log("Saving catalog-info.yaml");
+  console.log("========================");
+  saveCatalogInfo(catalogInfoContent)
+  // parse catalog-info.yaml
+  console.log("Parsing catalog-info.yaml");
+  console.log("=========================");
+  const parsedCatalogInfo = await parseCatalogInfo();
+  console.log(parsedCatalogInfo);
+
+  // check if apiVersion is the same as the current version
+  console.log("Checking apiVersion");
+  console.log("===================");
+  const apiVersion = parsedCatalogInfo.apiVersion;
+  console.log(apiVersion);
+
+  console.log("Deleting catalog-info.yaml");
+  fs.rmSync("catalog-info.yaml");
+};
+
+run();
---
+++ src/action.test.js
@@ -0,0 +1,165 @@
+"use strict";
+
+const core = require("@actions/core");
+const github = require("@actions/github");
+require("@octokit/auth-app");
+const { when } = require("jest-when");
+
+const { action } = require("./action.js");
+
+const { queryRepository, queryTeamsForRepository } = require("./query.js");
+const {
+  hasCatalogInfo,
+  generateCatalogInfo,
+  saveCatalogInfo,
+} = require("./catalog.js");
+
+jest.mock("@actions/core");
+jest.mock("@actions/github", () => ({
+  context: {
+    repo: {
+      owner: "owner",
+      repo: "repo",
+    },
+  },
+  getOctokit: jest.fn(),
+}));
+jest.mock("@octokit/auth-app", () => ({
+  createAppAuth: () => () => ({
+    type: "app",
+    token: "token",
+  }),
+}));
+jest.mock("./query.js");
+jest.mock("./catalog.js");
+
+describe("action", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test("default flow", async () => {
+    const repoData = {
+      id: "123456",
+      name: "repo",
+      full_name: "owner/repo",
+      description: "repo description",
+      owner: {
+        login: "owner",
+      },
+    };
+
+    const teamsData = [
+      {
+        name: "team1 name",
+        id: "123",
+        slug: "team1-name",
+        description: "team1 description",
+        permission: "maintain",
+      },
+      {
+        name: "team2 name",
+        id: "456",
+        slug: "team2-name",
+        description: "team2 description",
+        permission: "admin",
+      },
+    ];
+
+    when(core.getInput)
+      .calledWith("github-app-id")
+      .mockReturnValue("github-app-id");
+
+    when(core.getInput)
+      .calledWith("github-app-installation-id")
+      .mockReturnValue("github-app-installation-id");
+
+    when(core.getInput)
+      .calledWith("github-app-private-key")
+      .mockReturnValue("github-app-private-key");
+
+    when(github.getOctokit).calledWith("token").mockReturnValue("octokit");
+
+    when(queryRepository)
+      .calledWith("octokit", "owner", "repo")
+      .mockReturnValue(repoData);
+
+    when(queryTeamsForRepository)
+      .calledWith("octokit", "owner", "repo")
+      .mockReturnValue(teamsData);
+
+    when(hasCatalogInfo).calledWith().mockReturnValue(false);
+
+    const catalogInfoData = {
+      apiVersion: "backstage.io/v1alpha1",
+      kind: "Component",
+      metadata: {
+        name: "repo",
+        description: "repo description",
+      },
+      spec: {
+        type: "website",
+        lifecycle: "experimental",
+        owner: "team2-name",
+      },
+    };
+
+    when(generateCatalogInfo)
+      .calledWith("repository", "teams")
+      .mockReturnValue(catalogInfoData);
+
+    await action();
+
+    expect(queryRepository).toHaveBeenCalledWith("octokit", "owner", "repo");
+    expect(queryTeamsForRepository).toHaveBeenCalledWith(
+      "octokit",
+      "owner",
+      "repo",
+    );
+    expect(hasCatalogInfo).toReturnWith(false);
+
+    expect(generateCatalogInfo).toHaveBeenCalledWith(repoData, teamsData);
+
+    expect(saveCatalogInfo).toHaveBeenCalled();
+  });
+
+  test("catalog-info.yaml already exists", async () => {
+    when(core.getInput)
+      .calledWith("github-app-id")
+      .mockReturnValue("github-app-id");
+
+    when(core.getInput)
+      .calledWith("github-app-installation-id")
+      .mockReturnValue("github-app-installation-id");
+
+    when(core.getInput)
+      .calledWith("github-app-private-key")
+      .mockReturnValue("github-app-private-key");
+
+    when(github.getOctokit).calledWith("token").mockReturnValue("octokit");
+
+    when(queryRepository)
+      .calledWith("octokit", "owner", "repo")
+      .mockReturnValue("repository");
+
+    when(queryTeamsForRepository)
+      .calledWith("octokit", "owner", "repo")
+      .mockReturnValue("teams");
+
+    when(hasCatalogInfo).calledWith().mockReturnValue(true);
+
+    await action();
+
+    expect(queryRepository).toHaveBeenCalledWith("octokit", "owner", "repo");
+    expect(queryTeamsForRepository).toHaveBeenCalledWith(
+      "octokit",
+      "owner",
+      "repo",
+    );
+    expect(hasCatalogInfo).toReturnWith(true);
+
+    expect(generateCatalogInfo).not.toHaveBeenCalled();
+
+    expect(saveCatalogInfo).not.toHaveBeenCalled();
+  });
+});
